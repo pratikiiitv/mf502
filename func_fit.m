081e2a868c3c34db78dcd89b89194469f9b90ec9
Pratik Shah <67506267+pratikiiitv@users.noreply.github.com>
2025-06-26 16:28:58 +0530

    Add files via upload

---
+++ func_fit.m
@@ -0,0 +1,36 @@
+clear all;
+close all;
+
+% Sinusoid with noise added
+fs = 44100;
+t = 0:1/fs:1;
+f = 440;
+x_ = sin(2*pi*f*t);
+x = x_ + 0.1*randn(1,length(t));
+N = length(x);
+
+% Construct matrix b
+k = 20;
+b = x_(k:N)';
+% Construct matrix A
+A = [];
+for i = k:N 
+    A = [A;x(i:-1:i-k+1)];
+end 
+
+% Pseudo Inverse Filter Design
+a = A\b; % inv(A'*A)*A'*b
+
+% Moving Average Filter
+
+y(1:k-1) = x(1:k-1);
+z(1:k-1) = x(1:k-1);
+for i = k:N-k
+    y(i) = sum(x(i-k+1:i))/k;
+    z(i) = x(i:-1:i-k+1)*a;
+end 
+
+figure;
+plot(x_(1:1000)); hold on;
+plot(y(1:1000),'g');
+plot(z(1:1000),'r');
